fd6787587f9b6e377769cb9cfb2b5cd058d7f2ba
samuel00100 <101049839+samuel00100@users.noreply.github.com>
2022-04-08 15:32:32 +0200

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,39 +1,19 @@
-local dens = 0.0
-local zero = true
 local playerPed = GetPlayerPed(-1)
 local playerLocalisation = GetEntityCoords(playerPed)
-
-RegisterCommand("density", function(source, args, rawCommand)
-    TriggerEvent('chat:addSuggestion', '/density', '0.0 = none; 1.0 = max', {
-        { name="0.0 - 1.0", help="none - max" }
-    })
-    dens = args[1]
-    if args[1] == 0.0 then
-        zero = false
-    end
-    TriggerEvent('chat:addMessage', {
-        color = { 255, 0, 0},
-        multiline = true,
-        args = {"Density: ", dens}
-      })
-      
-end)    
 
 Citizen.CreateThread (function()
     while true do
         Citizen.Wait(0)
-        SetParkedVehicleDensityMultiplierThisFrame (dens)
-        SetVehicleDensityMultiplierThisFrame (dens)
-        SetRandomVehicleDensityMultiplierThisFrame(dens)
-        SetPedDensityMultiplierThisFrame(dens)
-        SetScenarioPedDensityMultiplierThisFrame(dens)
-        SetCreateRandomCops(zero)
-	SetCreateRandomCopsNotOnScenarios(zero)
-	SetCreateRandomCopsOnScenarios(zero)
-	SetGarbageTrucks(zero)
-	SetRandomBoats(zero)
-	if dens == 0.0 then
-        	ClearArea(playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, 1000.0, true, false, false, false)
-	end
+        SetParkedVehicleDensityMultiplierThisFrame (0.0)
+        SetVehicleDensityMultiplierThisFrame (0.0)
+        SetRandomVehicleDensityMultiplierThisFrame(0.0)
+        SetPedDensityMultiplierThisFrame(0.0)
+        SetScenarioPedDensityMultiplierThisFrame(0.0)
+        SetCreateRandomCops(false)
+		SetCreateRandomCopsNotOnScenarios(false)
+		SetCreateRandomCopsOnScenarios(false)
+		SetGarbageTrucks(false)
+		SetRandomBoats(false)
+        ClearArea(playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, 1000.0, true, false, false, false)
     end
 end)
